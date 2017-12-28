f7425b28a62aadc99eb2ce6e24189a17ad2a6f5d
Fernando Espinosa <fjevictoriano@gmail.com>
2017-12-27 20:50:00 -0400

    add matching,mismatching effects

--- js/app.js
+++ js/app.js
@@ -1,113 +1,161 @@
 'use strict'
+let Grid = function() {
+  var numberOfMoves = 0;
+  var numberOfClicks = 0;
+  var previewsFlippedCard = {};
 
-var Card = function(id, icon) {
+  let grid = document.querySelector(".grid");
+  this.cards = [];
+
+  grid.fillWithCards = function(cards) {
+    for (var i = 0; i < 16; i++) {
+      grid.appendChild(cards[i]);
+    }
+    this.cards = cards;
+  };
+
+  grid.clear = function() {
+    grid.innerHTML = "";
+  };
+
+  grid.shuffle = function() {
+    this.clear();
+    var i = 0,
+      j = 0,
+      temp = null
+    for (i = this.cards.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = this.cards[i]
+      this.cards[i] = this.cards[j]
+      this.cards[j] = temp
+    }
+    this.fillWithCards(this.cards);
+  };
+
+  let animate = function(options, ...cards) {
+    let type = options["type"];
+    let afterDone = options["afterDone"]
+    if (type && afterDone) {
+      $(cards).animateCss(type, afterDone.bind(null, cards));
+    } else {
+      $(cards).animateCss(type);
+    }
+  };
+
+  $(grid).click(function(event) {
+    let currentFlippedCard = event.target.parentElement;
+    if (isNotCard(currentFlippedCard)) {
+      return;
+    }
+    if(currentFlippedCard.matches(".matched")){
+      return;
+    }
+
+    numberOfClicks++;
+    numberOfMoves++;
+    if (numberOfClicks > 1) {
+      let cardsMatched = checkIfMatch(currentFlippedCard, previewsFlippedCard);
+      if (cardsMatched) {
+        $([previewsFlippedCard, currentFlippedCard]).addClass("matched");
+
+        animate({
+          type: "rubberBand"
+        }, currentFlippedCard, previewsFlippedCard);
+
+      } else {
+        $([previewsFlippedCard.back, currentFlippedCard.back]).addClass("mismatch");
+
+        animate({
+          type: "wobble",
+          afterDone: resetCards
+        }, previewsFlippedCard, currentFlippedCard);
+      }
+      numberOfClicks = 0;
+    }
+    previewsFlippedCard = currentFlippedCard; //save for the second flip
+  });
+
+  let addClass = function(marker, ...cards) {
+    $(cards).addClass(marker);
+  };
+
+  let isNotCard = function(element) {
+    return (element && !element.matches(".card"));
+  };
+
+  let resetCards = function(cards) {
+    $([cards[0].back, cards[1].back]).removeClass("mismatch");
+    $(cards).flip(false);
+    $(cards).one("click", function (){
+      $(this).flip(true);
+    });
+  };
+
+  let checkIfMatch = function(currentCard, previewsCard) {
+    let cardsMatched = false;
+    if (currentCard.icon === previewsCard.icon) {
+      cardsMatched = true;
+    }
+    return cardsMatched;
+  };
+
+
+  return grid;
+};
+
+let Card = function(id, icon) {
   let card = document.createElement("div");
   card.icon = icon;
-  card.id = "card-" + id;
+  card.id = `card-${id}`;
   card.setAttribute("class", "card");
   let front = document.createElement("div");
+  front.id = `card-front-${id}`;
   front.setAttribute("class", "front");
   let iconTag = document.createElement("i");
-  iconTag.setAttribute("class", "fa fa-3x " + icon);
+  iconTag.setAttribute("class", "fa fa-3x card-icon " + icon);
   let back = document.createElement("div");
   back.appendChild(iconTag);
   back.setAttribute("class", "back");
+  back.id = `card-back-${id}`;
+  card.front = front;
+  card.back = back;
   card.appendChild(front);
   card.appendChild(back);
 
   card.isFlipped = function() {
-    return card.classList.contains("flipped");
-  };
-
-
-  let onClickEventHanlder = function(event) {
-
-  };
-
-  let onFlipDoneEventHandler = function(event) {
-    markCardAsFlipped();
-    let currentCard = event.currentTarget;
-    if (checkIfMatch(currentCard)) {
-          
-    }
-    if (!checkIfMatch(currentCard)) {
-
-    }
-  };
-
-  let markCardAsFlipped = function() {
-    let classList = card.classList;
-    classList.contains("flipped") ? classList.remove("flipped") :
-      classList.add("flipped");
+    return $(card).data("flip-model").isFlipped;
   };
 
   $(card).flip({
-    trigger: "click"
+    trigger: "manual",
+    speed: 100
   });
-  $(card).click(onClickEventHanlder);
-  $(card).on("flip:done", onFlipDoneEventHandler);
+
+  $(card).one("click", function (){
+    $(this).flip(true);
+  });
 
   return card;
 };
 
-let checkIfMatch = function(card) {
-  let matchFound = false;
-  let cards = document.querySelectorAll(".card");
-  cards.forEach(c => {
-
-    if (c.isFlipped() && c.id != card.id && (c.icon === card.icon)) {
-      matchFound = true;
-    }
-  });
-  return matchFound;
-};
-
 let createCards = function() {
   let cards = [];
-  const icons = ["fa-moon-o", "fa-bicycle", "fa-space-shuttle", "fa-usb",
-    "fa-user-md", "fa-superpowers", "fa-university", "fa-sign-language"
-  ];
-
+  const icons = ["fa-moon-o", "fa-bicycle", "fa-space-shuttle", "fa-usb", "fa-user-md", "fa-superpowers", "fa-university", "fa-sign-language"];
   let cardId = 1;
-  for (var i = 0; i < 2; i++) {
-    for (var j = 0; j < 8; j++) {
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 8; j++) {
       let card = new Card(cardId++, icons[j]);
       cards.push(card);
     }
   }
   return cards;
-}
-
-
-function shuffle(cards) {
-  var i = 0,
-    j = 0,
-    temp = null
-
-  for (i = cards.length - 1; i > 0; i -= 1) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = cards[i]
-    cards[i] = cards[j]
-    cards[j] = temp
-  }
-
 };
 
-function fillGridWithCards(cards) {
-  let grid = document.querySelector(".grid");
-  for (var i = 0; i < 16; i++) {
-    grid.appendChild(cards[i]);
-  }
-}
-
-let cards = createCards();
-fillGridWithCards(cards);
-
-document.querySelector("button[name=shuffle]").addEventListener("click", function() {
-  let grid = document.querySelector(".grid");
-  grid.innerHTML = "";
-  shuffle(cards);
-  fillGridWithCards(cards);
+var grid = new Grid();
+var cards = createCards();
+grid.fillWithCards(cards);
+document.querySelector(".fa-repeat").addEventListener("click", function() {
+  grid.shuffle();
 
 
 });
